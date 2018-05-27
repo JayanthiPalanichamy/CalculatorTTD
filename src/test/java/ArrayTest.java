bbdabc792b74e27a32dabd46c65fb91520e4698c
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-05-27 20:44:32 +0530

    Refactoring redundant code

--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -1,4 +1,5 @@
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -7,11 +8,14 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class ArrayTest {
+    List<Integer> arrayList;
+    @Before
+    public void setUp(){
+        arrayList = Arrays.asList(1,100,0,2,3);
 
+    }
     @Test
     public void returnMinAsZeroFromGivenArray() {
-        List<Integer> arrayList = Arrays.asList(1,100,0,2,3);
-
         assertEquals(0,CalculatorArray.minValue(arrayList));
     }
 }
