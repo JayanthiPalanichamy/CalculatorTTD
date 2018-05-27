cba5765a7ab77bca818cf9ce05ed7be384d85304
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-05-27 20:36:09 +0530

    Initializing TTD with calculator of array

---
+++ README.md
@@ -0,0 +1,9 @@
+Given [100, -1, 9, 4 , 11] to  Calculator, it should be able to give 
+
+Minimum value = -1
+
+Maximum value = 100
+
+Average = (123/5) = 24.6
+
+Using Test Driven Development
---
+++ src/test/java/ArrayTest.java
@@ -0,0 +1,13 @@
+package java;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayTest {
+
+    @Test
+    public void returnMinAsZeroFromGivenArray() {
+        List<Integer> arrayList = Arrays.asList(1,100,0,2,3);
+    }
+}
