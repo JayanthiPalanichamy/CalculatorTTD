58419ded94175ee69390718eda82088a63298408
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-05-27 20:50:13 +0530

    Implementation of minValue Function

--- src/main/java/CalculatorArray.java
+++ src/main/java/CalculatorArray.java
@@ -1,7 +1,12 @@
+import java.util.Comparator;
 import java.util.List;
 
 public class CalculatorArray {
     public static int minValue(List<Integer> arrayList) {
+        return arrayList.stream().min(Comparator.naturalOrder()).orElse(0);
+    }
+
+    public static int maxValue(List<Integer> arrayList2) {
         return 0;
     }
 }
--- src/test/java/ArrayTest.java
+++ src/test/java/ArrayTest.java
@@ -8,14 +8,28 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class ArrayTest {
-    List<Integer> arrayList;
+    List<Integer> arrayList1;
+    List<Integer> arrayList2;
+
     @Before
     public void setUp(){
-        arrayList = Arrays.asList(1,100,0,2,3);
-
+        arrayList1 = Arrays.asList(1,10,0,2,3);
+        arrayList2 = Arrays.asList(100,-1,9,4,11);
     }
+
     @Test
     public void returnMinAsZeroFromGivenArray() {
-        assertEquals(0,CalculatorArray.minValue(arrayList));
+        assertEquals(0,CalculatorArray.minValue(arrayList1));
+    }
+
+    @Test
+    public void returnMinValueMinusOneFromGivenArray() {
+        assertEquals(-1,CalculatorArray.minValue(arrayList2));
+
+    }
+
+    @Test
+    public void returnHundredAsMaxValueFromGivenArray() {
+        assertEquals(100,CalculatorArray.maxValue(arrayList2));
     }
 }
